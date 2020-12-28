78ad1a25311d28d4b835150c09fd07b325fdcb45
DivijSanjanwala <59158971+DivijSanjanwala@users.noreply.github.com>
2020-12-28 02:57:49 -0500

    Briefing to yfinance library and sample cleaning data

--- main_yahoo.py
+++ main_yahoo.py
@@ -1,8 +1,32 @@
-import yfinance as yf
-import numpy as np
 import pandas as pd
+import numpy as np
+import yfinance as yf
 
-tickerSymbol = 'MSFT'
+# Ticker symbol for each company
+tickerSymbol = 'TSLA'
 tickerData = yf.Ticker(tickerSymbol)
+# Sample Query to obtain the stock data
 tickerDf = tickerData.history(period='1d', start='2010-1-1', end='2020-12-31')
-print(tickerDf.columns)
+
+
+def data_clean(ticker_dataframe: pd.core):
+    """This function returns a None type and cleans the DataFrame object by
+    doing the following.
+    1. Cleaning the NaN values of the DataFrame object
+    2. Rounding the Opening Stock Prices and the Closing Stock Prices
+       to 3 decimal places.
+    3. add as needed
+    """
+    # TODO
+    ticker_dataframe.dropna()
+    ticker_dataframe['Open'] = np.round(ticker_dataframe['Open'], 3)
+    ticker_dataframe['Close'] = np.round(ticker_dataframe['Close'], 3)
+
+    return ticker_dataframe
+
+
+# *****************************************************************************
+# Testing the function
+print(data_clean(tickerDf).head())
+print(tickerDf.head())
+# *****************************************************************************
